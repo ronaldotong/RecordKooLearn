488925e05ed03ba5013bbad53e7e18149f821111
tongr <ronaldo.tong@autodesk.com>
2017-11-21 14:16:56 +0800

    Add Command Process

--- RecordKooLearn/Form1.Designer.cs
+++ RecordKooLearn/Form1.Designer.cs
@@ -28,24 +28,127 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            this.labelCurrentPage = new System.Windows.Forms.Label();
+            this.textBoxCurrentURL = new System.Windows.Forms.TextBox();
+            this.labelNextUrl = new System.Windows.Forms.Label();
+            this.textBoxNextURL = new System.Windows.Forms.TextBox();
+            this.textBoxTimeDur = new System.Windows.Forms.TextBox();
+            this.labelTimeDuration = new System.Windows.Forms.Label();
+            this.buttonNextPage = new System.Windows.Forms.Button();
+            this.buttonBeginRecord = new System.Windows.Forms.Button();
+            this.buttonStopRecord = new System.Windows.Forms.Button();
+            this.buttonGo = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
-            // label1
+            // labelCurrentPage
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(33, 57);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.labelCurrentPage.AutoSize = true;
+            this.labelCurrentPage.Location = new System.Drawing.Point(33, 57);
+            this.labelCurrentPage.Name = "labelCurrentPage";
+            this.labelCurrentPage.Size = new System.Drawing.Size(66, 13);
+            this.labelCurrentPage.TabIndex = 0;
+            this.labelCurrentPage.Text = "Current URL";
+            // 
+            // textBoxCurrentURL
+            // 
+            this.textBoxCurrentURL.Location = new System.Drawing.Point(123, 49);
+            this.textBoxCurrentURL.Name = "textBoxCurrentURL";
+            this.textBoxCurrentURL.Size = new System.Drawing.Size(484, 20);
+            this.textBoxCurrentURL.TabIndex = 1;
+            // 
+            // labelNextUrl
+            // 
+            this.labelNextUrl.AutoSize = true;
+            this.labelNextUrl.Location = new System.Drawing.Point(36, 145);
+            this.labelNextUrl.Name = "labelNextUrl";
+            this.labelNextUrl.Size = new System.Drawing.Size(45, 13);
+            this.labelNextUrl.TabIndex = 2;
+            this.labelNextUrl.Text = "Next Url";
+            // 
+            // textBoxNextURL
+            // 
+            this.textBoxNextURL.Location = new System.Drawing.Point(123, 138);
+            this.textBoxNextURL.Name = "textBoxNextURL";
+            this.textBoxNextURL.Size = new System.Drawing.Size(484, 20);
+            this.textBoxNextURL.TabIndex = 3;
+            // 
+            // textBoxTimeDur
+            // 
+            this.textBoxTimeDur.Location = new System.Drawing.Point(123, 230);
+            this.textBoxTimeDur.Name = "textBoxTimeDur";
+            this.textBoxTimeDur.Size = new System.Drawing.Size(484, 20);
+            this.textBoxTimeDur.TabIndex = 4;
+            // 
+            // labelTimeDuration
+            // 
+            this.labelTimeDuration.AutoSize = true;
+            this.labelTimeDuration.Location = new System.Drawing.Point(36, 236);
+            this.labelTimeDuration.Name = "labelTimeDuration";
+            this.labelTimeDuration.Size = new System.Drawing.Size(73, 13);
+            this.labelTimeDuration.TabIndex = 5;
+            this.labelTimeDuration.Text = "Time Duration";
+            // 
+            // buttonNextPage
+            // 
+            this.buttonNextPage.Location = new System.Drawing.Point(123, 333);
+            this.buttonNextPage.Name = "buttonNextPage";
+            this.buttonNextPage.Size = new System.Drawing.Size(75, 23);
+            this.buttonNextPage.TabIndex = 6;
+            this.buttonNextPage.Text = "Next Page";
+            this.buttonNextPage.UseVisualStyleBackColor = true;
+            this.buttonNextPage.Click += new System.EventHandler(this.buttonNextPage_Click);
+            // 
+            // buttonBeginRecord
+            // 
+            this.buttonBeginRecord.Location = new System.Drawing.Point(273, 333);
+            this.buttonBeginRecord.Name = "buttonBeginRecord";
+            this.buttonBeginRecord.Size = new System.Drawing.Size(119, 23);
+            this.buttonBeginRecord.TabIndex = 7;
+            this.buttonBeginRecord.Text = "Begin Record";
+            this.buttonBeginRecord.UseVisualStyleBackColor = true;
+            this.buttonBeginRecord.Click += new System.EventHandler(this.buttonBeginRecord_Click);
+            // 
+            // buttonStopRecord
+            // 
+            this.buttonStopRecord.Location = new System.Drawing.Point(453, 333);
+            this.buttonStopRecord.Name = "buttonStopRecord";
+            this.buttonStopRecord.Size = new System.Drawing.Size(119, 23);
+            this.buttonStopRecord.TabIndex = 8;
+            this.buttonStopRecord.Text = "Stop Record";
+            this.buttonStopRecord.UseVisualStyleBackColor = true;
+            this.buttonStopRecord.Click += new System.EventHandler(this.buttonStopRecord_Click);
+            // 
+            // buttonGo
+            // 
+            this.buttonGo.Location = new System.Drawing.Point(123, 95);
+            this.buttonGo.Name = "buttonGo";
+            this.buttonGo.Size = new System.Drawing.Size(75, 23);
+            this.buttonGo.TabIndex = 9;
+            this.buttonGo.Text = "Go";
+            this.buttonGo.UseVisualStyleBackColor = true;
+            this.buttonGo.Click += new System.EventHandler(this.buttonGo_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // FormMainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(661, 434);
+            this.Controls.Add(this.buttonGo);
+            this.Controls.Add(this.buttonStopRecord);
+            this.Controls.Add(this.buttonBeginRecord);
+            this.Controls.Add(this.buttonNextPage);
+            this.Controls.Add(this.labelTimeDuration);
+            this.Controls.Add(this.textBoxTimeDur);
+            this.Controls.Add(this.textBoxNextURL);
+            this.Controls.Add(this.labelNextUrl);
+            this.Controls.Add(this.textBoxCurrentURL);
+            this.Controls.Add(this.labelCurrentPage);
             this.Name = "FormMainWindow";
             this.Text = "FormMainWindow";
             this.ResumeLayout(false);
@@ -55,7 +158,17 @@
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label labelCurrentPage;
+        private System.Windows.Forms.TextBox textBoxCurrentURL;
+        private System.Windows.Forms.Label labelNextUrl;
+        private System.Windows.Forms.TextBox textBoxNextURL;
+        private System.Windows.Forms.TextBox textBoxTimeDur;
+        private System.Windows.Forms.Label labelTimeDuration;
+        private System.Windows.Forms.Button buttonNextPage;
+        private System.Windows.Forms.Button buttonBeginRecord;
+        private System.Windows.Forms.Button buttonStopRecord;
+        private System.Windows.Forms.Button buttonGo;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- RecordKooLearn/Form1.cs
+++ RecordKooLearn/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,5 +18,82 @@ namespace RecordKooLearn
             InitializeComponent();
         }
 
+        private void buttonNextPage_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBeginRecord_Click(object sender, EventArgs e)
+        {
+            string dur = textBoxTimeDur.Text;
+            string exe = "\"C:\\Program Files\\TechSmith\\Camtasia 9\\CamRecorder.exe\"";
+            string arg = @"/record";
+            string[] args = { exe, arg };
+            StartProcess("cmd.exe", args);
+            int min = int.Parse(dur) + 1;
+            int sec = min * 60;
+            int mileSec = sec * 1000;
+            timer1.Interval = mileSec;
+            timer1.Start();
+        }
+
+        private void buttonStopRecord_Click(object sender, EventArgs e)
+        {
+            stop_record();
+        }
+
+        private void buttonGo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool StartProcess(string filename, string[] args)
+        {
+            try
+            {
+                string s = @" ";
+                foreach (string arg in args)
+                {
+                    s = s   + arg + " ";
+                }
+                s = s.Trim();
+
+                Process.Start(@"cmd", @"/c " + s);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("启动应用程序时出错！原因：" + ex.Message);
+            }
+            return false;
+        }
+
+        public void executeCmd(string filename, string[] args)
+        {
+            Process p = new Process();
+
+            p.StartInfo.FileName = "cmd.exe";           //設定程序名
+            p.StartInfo.Arguments = @"/c " + filename;    //設定程式執行參數
+            p.StartInfo.UseShellExecute = true;        //關閉Shell的使用
+            p.StartInfo.RedirectStandardInput = true;   //重定向標準輸入
+            p.StartInfo.RedirectStandardOutput = true;  //重定向標準輸出
+            p.StartInfo.RedirectStandardError = true;   //重定向錯誤輸出
+            p.StartInfo.CreateNoWindow = true;          //設置不顯示窗口
+            p.Start();   //啟動
+        }
+
+        private void stop_record()
+        {
+            string exe = @"C:\Program Files\TechSmith\Camtasia 9\CamtasiaStudio.exe";
+            string arg = @"/stop";
+            string[] args = { exe, arg };
+            StartProcess("cmd.exe", args);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            stop_record();
+            timer1.Stop();
+        }
     }
 }
